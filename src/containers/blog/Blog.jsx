1505bbc7f86d27c5813e79d6b694659663b03ace
blackgram <ajiriokpeva@gmail.com>
2023-02-08 11:48:51 +0100

    last commit

---
+++ src/containers/blog/Blog.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './blog.css'
+import { Article } from '../../components'
+import { blog01, blog02, blog03, blog04, blog05, blogPosts} from './imports'
+
+const Blog = () => {
+  return (
+    <div className="gpt3__blog section__padding" id='#blog'>
+      <div className="gpt3__blog-heading">
+        <h1 className="gradient__text">A lot is happening, We are blogging about it.</h1>
+      </div>
+      <div className="gpt3__blog-container">
+        <div className="gpt3__blog-container_groupA">
+          <Article imgUrl={blog01} date={blogPosts.date} title={blogPosts.title} />
+        </div>
+        <div className="gpt3__blog-container_groupB">
+        <Article imgUrl={blog02} date={blogPosts.date} title={blogPosts.title} />
+        <Article imgUrl={blog03} date={blogPosts.date} title={blogPosts.title} />
+        <Article  imgUrl={blog04} date={blogPosts.date} title={blogPosts.title} />
+        <Article imgUrl={blog05} date={blogPosts.date} title={blogPosts.title} />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Blog
---
+++ src/containers/blog/imports.js
@@ -0,0 +1,45 @@
+import blog01 from '../../assets/blog01.png'
+import blog02 from '../../assets/blog02.png'
+import blog03 from '../../assets/blog03.png'
+import blog04 from '../../assets/blog04.png'
+import blog05 from '../../assets/blog05.png'
+
+const blogPosts = (
+    {
+        id:1,
+        date: "Jan 23, 2023",
+        title: "GPT-3 and Open  AI is the future. Let us exlore how it is?"
+    },
+    {
+        id:2,
+        date: "Feb 24, 2023",
+        title: "ChatGPT and its very useful applications"
+    },
+    {
+        id:3,
+        date: "March 25, 2023" ,
+        title: "Working in tech as a Nigerian and its downsides"
+    },
+    {
+        id:4,
+        date: "April 26, 2023",
+        title: "Will chatGPT and other AI replace junior developers in the nearest future?"
+    },
+    {
+        id:5,
+        date: "May 27, 2023" ,
+        title: "Discovering my talents and prospective skills while growing"
+    }
+)
+
+
+
+
+export {
+    blog01,
+    blog02,
+    blog03,
+    blog04,
+    blog05,
+    blogPosts
+}
